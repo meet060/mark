3306dac91f5d228707fe3ffea3405ffbcc1f7f87
hanguoli <383018389@qq.com>
2020-04-19 22:22:00 +0800

    图片显示

--- src/main/java/com/xingyue/controller/WebMvcConfig.java
+++ src/main/java/com/xingyue/controller/WebMvcConfig.java
@@ -3,42 +3,16 @@ package com.xingyue.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * 路径映射配置
- */
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfig implements WebMvcConfigurer {
 
-	/*
-	 * @Value("${config.bpfeaturedfirstmanagement.advert}") private String advert;
-	 * 
-	 * @Value("${config.imgNewUrl}") private String imgNewUrl;
-	 * 
-	 * @Value("${config.ftp_path}") private String ftp_path;
-	 */
-	
-    @Value("${package.storage}")
-    private String storagePath;
-
-	/**
-	 * 图片视频路径映射配置
-	 * 
-	 * @param registry
-	 */
+	@Value("${package.storage}")
+	private String storagePath;
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		// registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-		// 最新动态图片路径配置
-//		registry.addResourceHandler(imgNewUrl + "**").addResourceLocations("file:" + imgNewUrl);
-		// 知识管理视频图片
-//		registry.addResourceHandler(imgNewUrl + "image/**").addResourceLocations("file:" + imgNewUrl + "image/");
-//		registry.addResourceHandler("/opt/afin/ueditor/image/**").addResourceLocations("file:/opt/afin/ueditor/image/");
-		// FTP文件路径配置
-//		registry.addResourceHandler(ftp_path + "**").addResourceLocations("file:" + ftp_path);
-		// 推荐位图片路径配置
-		registry.addResourceHandler(storagePath + "/**").addResourceLocations("file: /" + storagePath);
-		super.addResourceHandlers(registry);
+		//  /home/file/**为前端URL访问路径  后面 file:xxxx为本地磁盘映射
+		registry.addResourceHandler(storagePath+"**").addResourceLocations("file:" + storagePath);
 	}
 }
