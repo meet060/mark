885ecefc548c157f7c3c64679fa76b0797099141
hanguoli <383018389@qq.com>
2020-04-22 14:45:56 +0800

    添加跨域请求

--- src/main/java/com/xingyue/config/WebMvcConfig.java
+++ src/main/java/com/xingyue/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.xingyue.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +16,19 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		// /home/file/**为前端URL访问路径 后面 file:xxxx为本地磁盘映射
 		registry.addResourceHandler(storagePath + "**").addResourceLocations("file:" + storagePath);
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		// 允许跨域访问的路径，全允许则设为 *
+		registry.addMapping("/**")
+				// 允许跨域访问的源
+                .allowedOrigins("*")
+				// 允许请求方法
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+				// 预检间隔时间
+                .maxAge(168000)
+				// 是否发送cookie
+                .allowCredentials(true);
 	}
 }
